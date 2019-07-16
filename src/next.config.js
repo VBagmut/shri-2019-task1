3042898347ab9300e8f33166eb4bafd59ea60d1d
Vyacheslav Bagmut <bagmutva@gmail.com>
2019-07-17 00:34:38 +0300

    Added glob for import in SCSS. Edited layout block.

--- src/next.config.js
+++ src/next.config.js
@@ -29,7 +29,8 @@ module.exports = {
                     }
                 },
                 'css-loader',
-                "sass-loader"
+                "sass-loader",
+                'import-glob'
             ]
         });
 
