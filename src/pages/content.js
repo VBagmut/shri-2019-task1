c92d8a6bef13c41538856525294a3624e84b971d
Vyacheslav Bagmut <bagmutva@gmail.com>
2019-07-21 01:48:29 +0300

    Added new blocks

--- src/pages/content.js
+++ src/pages/content.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import templatesData from "../templateData";
 import generateMarkup from "../template-engine";
 
-export default class IndexPage extends Component {
+export default class ContentPage extends Component {
     render() {
         return (
             <div
                 dangerouslySetInnerHTML={{
-                    __html: generateMarkup(templatesData.index)
+                    __html: generateMarkup(templatesData.content)
                 }}
             />
         );
--- src/templateData.js
+++ src/templateData.js
@@ -1,62 +1,218 @@
 module.exports = {
     index: [
         {
-            block: "layout",
-            elem: "container",
-            elemMods: { size: "m", active: true }
-        }
-    ],
-    grid: [
-        {
-            block: 'grid',
-            mods: { 'm-columns': '10'},
+            block: "theme",
+            mods: {
+                color: "project-default",
+                space: "default",
+                size: "default",
+                gap: "small"
+            },
             content: {
-                block: 'text',
-                content: 'lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000lorem1000'
+                block: "layout",
+                content: {
+                    elem: "container",
+                    elemMods: {
+                        size: "m",
+                        align: "center"
+                    },
+                    content: [
+                        {
+                            block: "section",
+                            mods: {
+                                "indent-b": "xxl"
+                            },
+                            content: [
+                                {
+                                    block: "grid",
+                                    mods: {
+                                        "m-columns": 10,
+                                        "col-gap": "full"
+                                    },
+                                    content: [
+                                        {
+                                            block: "payment",
+                                            mix: [
+                                                {
+                                                    block: "grid",
+                                                    elem: "fraction",
+                                                    elemMods: {
+                                                        "m-col": 5
+                                                    }
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            block: "warning",
+                                            mix: [
+                                                {
+                                                    block: "grid",
+                                                    elem: "fraction",
+                                                    elemMods: {
+                                                        "m-col": 5
+                                                    }
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            block: "section",
+                            mods: {
+                                "indent-b": "xxl"
+                            },
+                            content: [
+                                {
+                                    block: "grid",
+                                    mods: {
+                                        "m-columns": 10,
+                                        "col-gap": "half"
+                                    },
+                                    content: [
+                                        {
+                                            block: "product",
+                                            mix: [
+                                                {
+                                                    block: "grid",
+                                                    elem: "fraction",
+                                                    elemMods: {
+                                                        "m-col": 2
+                                                    }
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            block: "product",
+                                            mix: [
+                                                {
+                                                    block: "grid",
+                                                    elem: "fraction",
+                                                    elemMods: {
+                                                        "m-col": 2
+                                                    }
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            block: "product",
+                                            mix: [
+                                                {
+                                                    block: "grid",
+                                                    elem: "fraction",
+                                                    elemMods: {
+                                                        "m-col": 2
+                                                    }
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            block: "product",
+                                            mix: [
+                                                {
+                                                    block: "grid",
+                                                    elem: "fraction",
+                                                    elemMods: {
+                                                        "m-col": 2
+                                                    }
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            block: "product",
+                                            mix: [
+                                                {
+                                                    block: "grid",
+                                                    elem: "fraction",
+                                                    elemMods: {
+                                                        "m-col": 2
+                                                    }
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            block: "section",
+                            mods: {
+                                "indent-b": "xxl"
+                            },
+                            content: [
+                                {
+                                    block: "grid",
+                                    mods: {
+                                        "m-columns": 12,
+                                        "col-gap": "full"
+                                    },
+                                    content: [
+                                        {
+                                            block: "card",
+                                            mods: {
+                                                border: "all"
+                                            },
+                                            mix: [
+                                                {
+                                                    block: "grid",
+                                                    elem: "fraction",
+                                                    elemMods: {
+                                                        "m-col": 8
+                                                    }
+                                                }
+                                            ],
+                                            content: "List"
+                                        },
+                                        {
+                                            block: "card",
+                                            mods: {
+                                                border: "all"
+                                            },
+                                            mix: [
+                                                {
+                                                    block: "grid",
+                                                    elem: "fraction",
+                                                    elemMods: {
+                                                        "m-col": 4
+                                                    }
+                                                }
+                                            ],
+                                            content: [
+                                                {
+                                                    block: "text",
+                                                    mods: {
+                                                        size: "m",
+                                                        type: "p",
+                                                        view: "primary"
+                                                    },
+                                                    content: [
+                                                        {
+                                                            elem: "word",
+                                                            elemMods: {
+                                                                width: "l"
+                                                            }
+                                                        },
+                                                        {
+                                                            elem: "word",
+                                                            elemMods: {
+                                                                width: "m"
+                                                            }
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
             }
         }
     ],
     product: [],
     content: [],
     collect: [],
-    // content blocks
-    payment: [
-        {
-            block: "layout",
-            elem: "container",
-            elemMods: { size: "m" },
-            content: {
-                block: "theme",
-                mods: {
-                    color: "project-default",
-                    space: "default_xl"
-                },
-                mix: { block: "payment" },
-                content: { 
-                        block: 'text',
-                        mods: {
-                            size: 'xxl',
-                            type: 'h1',
-                            view: 'primary',
-                        },
-                        content: {
-                            elem: 'word',
-                            mods: {
-                                size: 'xxl'
-                            },
-                            elemMods: {
-                                width: 'l'
-                            },
-                            
-                        },
-                                mix: {
-                                    block: 'theme',
-                                    mods: {
-                                        'size_default': 'xxl'
-                                    },
-                                }
-                 }
-            }
-        }
-    ]
 };
